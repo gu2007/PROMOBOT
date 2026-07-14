830f4aeef6682ae610d1a4c9bd58098034a3aaf9
Gustavo Noronha <gn20071402@gmail.com>
2026-07-13 23:25:30 -0300

    Fix: remove bloco duplicado do client

--- index.js
+++ index.js
@@ -83,14 +83,6 @@ const client = new Client({
     headless: true,
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
   },
-});
-
-const client = new Client({
-  authStrategy: new LocalAuth(), // guarda a sessão localmente, não precisa escanear QR toda vez
-  puppeteer: {
-    headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-  },
   webVersionCache: {
     type: 'remote',
     remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.3000.1031490220-alpha.html',
